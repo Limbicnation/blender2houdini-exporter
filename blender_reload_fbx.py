ebe7c4c82955f0bfb563c91bca149f8eedc10d03
gero <gero@limbicnation.com>
2023-05-04 19:26:57 +0200

    Add Blender addon to reload FBX file in Blender

--- blender_reload_fbx.py
+++ blender_reload_fbx.py
@@ -1,17 +1,15 @@
+bl_info = {
+    "name": "Reload FBX",
+    "author": "Your Name",
+    "version": (1, 0),
+    "blender": (3, 0, 0),
+    "location": "View3D > UI > Houdini",
+    "description": "Reload FBX file into the scene",
+    "category": "Object"
+}
+
 import bpy
 
-class MyPanel(bpy.types.Panel):
-    bl_label = "Reload FBX"
-    bl_idname = "OBJECT_PT_reload_fbx"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    bl_category = "Houdini"
-
-    def draw(self, context):
-        layout = self.layout
-        row = layout.row()
-        row.label(text="Click to reload FBX")
-        row.operator("object.reload_fbx")
 
 class ReloadFBXOperator(bpy.types.Operator):
     bl_idname = "object.reload_fbx"
@@ -42,13 +40,30 @@ class ReloadFBXOperator(bpy.types.Operator):
         
         return {'FINISHED'}
 
+
+class MyPanel(bpy.types.Panel):
+    bl_label = "Reload FBX"
+    bl_idname = "OBJECT_PT_reload_fbx"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Houdini"
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.label(text="Click to reload FBX")
+        row.operator("object.reload_fbx")
+
+
 def register():
-    bpy.utils.register_class(MyPanel)
     bpy.utils.register_class(ReloadFBXOperator)
+    bpy.utils.register_class(MyPanel)
+
 
 def unregister():
-    bpy.utils.unregister_class(MyPanel)
     bpy.utils.unregister_class(ReloadFBXOperator)
+    bpy.utils.unregister_class(MyPanel)
+
 
 if __name__ == "__main__":
     register()
