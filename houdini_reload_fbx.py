1675f290507db44e3760ae994d64847c2620342b
limbicnation <gero@limbicnation.com>
2023-06-12 23:27:27 +0200

    Fix houdini_reload_fbx.py script to handle file paths for both Windows and Linux

--- houdini_reload_fbx.py
+++ houdini_reload_fbx.py
@@ -1,7 +1,19 @@
 import hou
+import platform
 
-# Get the shelf named "MyShelf"
-my_shelf = hou.shelves.shelves()["houdini_importer"]
+# Get the file node named "file1" inside the 'geo1' node
+file_node = hou.node('/obj/geo1/file1')
+
+# Define the file path based on the operating system
+if platform.system() == 'Windows':
+    file_path = 'I:/Temp_geo/model.fbx'
+elif platform.system() == 'Linux':
+    file_path = '/media/ws-ml/linux-drive/linux_projects/Temp_geo/model.fbx'
+else:
+    raise OSError("Unsupported operating system.")
+
+# Set the file path parameter of the file node
+file_node.parm('file').set(file_path)
 
 # Press the Button to reload geometry
-hou.parm('/obj/geo1/file1/reload').pressButton()
+file_node.parm('reload').pressButton()
