f31eadc61e48d9b33d01e7ff911bd43e1f8ddc1a
limbicnation <gero@limbicnation.com>
2023-06-12 22:28:52 +0200

    Fix Houdini shelf script with corrected toolshelf definition and member tools

--- blender2houdini-exporter.py
+++ blender2houdini-exporter.py
@@ -15,48 +15,58 @@ import os
 import bpy
 import subprocess
 import re
+import platform
 
 
 class SendToHoudini(bpy.types.Operator):
     bl_idname = "object.send_houdini"
     bl_label = "Send To Houdini"
 
-    Houdinipath = 'C:/Program Files/Side Effects Software/Houdini 19.5.640/bin/houdinifx.exe'
-    HoudiniScript = 'I:/GitHub/Tools/blender2houdini-exporter/houdini_import_fbx.py'
-    export = None
-    
+    Houdinipath = ''
+    HoudiniScript = '/media/ws-ml/linux-drive/linux_projects/GitHub/blender2houdini-exporter/houdini_import_fbx.py'
+    geoPath = '/media/ws-ml/linux-drive/linux_projects/Temp_geo/model.fbx'
+
     def execute(self, context):
         # Verify the path and print it to the console
-        fbxPath = 'I:/Temp_geo/model.fbx'
-        print(f"Exporting to path: {fbxPath}")
-        
+        print(f"Exporting to path: {self.geoPath}")
+
         # Check that the export operator ID is correct
         print(f"Operator ID: {bpy.ops.export_scene.fbx.idname}")
-        
+
         # export the selected object to the specified path
-        bpy.ops.export_scene.fbx(filepath=fbxPath, use_selection=True, global_scale=1)
-        
+        bpy.ops.export_scene.fbx(filepath=self.geoPath, use_selection=True, global_scale=1)
+
         # Show a message box confirming the export
-        message = f"Exported to {fbxPath}"
+        message = f"Exported to {self.geoPath}"
         self.report({'INFO'}, message)
         print(message)
 
         # Check if Houdini is already running
         is_houdini_running = False
-        try:
-            output = subprocess.check_output('tasklist', shell=True)
-            output = output.decode('utf-8').lower()
-            is_houdini_running = re.search(r'houdinifx\.exe', output) is not None
-        except subprocess.CalledProcessError:
-            pass
-        
+
+        if platform.system() == 'Windows':
+            try:
+                output = subprocess.check_output('tasklist', shell=True)
+                output = output.decode('utf-8').lower()
+                is_houdini_running = re.search(r'houdinifx\.exe', output) is not None
+            except subprocess.CalledProcessError:
+                pass
+            houdini_path = r'C:\Program Files\Side Effects Software\Houdini 19.5.640\bin\houdinifx.exe'
+        elif platform.system() == 'Linux':
+            try:
+                output = subprocess.check_output('pgrep houdinifx', shell=True)
+                is_houdini_running = output.strip() != b''
+            except subprocess.CalledProcessError:
+                pass
+            houdini_path = '/opt/hfs19.5.640/bin/houdinifx'
+
         if not is_houdini_running:
             # launch Houdini
-            cmd = [self.Houdinipath, '-c', 'python', self.HoudiniScript]
+            cmd = [houdini_path, '-c', 'python', self.HoudiniScript]
             subprocess.Popen(cmd)
         else:
             print('Houdini is already running')
-        
+
         return {'FINISHED'}
 
 
@@ -66,7 +76,7 @@ class HOUDINI_PT_Panel(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'Houdini'
-    
+
     def draw(self, context):
         layout = self.layout
         layout.operator("object.send_houdini", text="Send To Houdini")
