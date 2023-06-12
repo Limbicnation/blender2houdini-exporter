09c955e139113c19de42928059daf234a84ed6b7
gero <gero@limbicnation.com>
2023-06-12 17:10:09 +0200

    Add Houdini launcher logic, use the correct Houdini path and Houdini script path. This commit improves the functionality of the Blender to Houdini exporter addon by preventing the opening of multiple instances of Houdini

--- blender2houdini-exporter.py
+++ blender2houdini-exporter.py
@@ -14,16 +14,17 @@ bl_info = {
 import os
 import bpy
 import subprocess
+import re
 
 
 class SendToHoudini(bpy.types.Operator):
     bl_idname = "object.send_houdini"
     bl_label = "Send To Houdini"
-    
-    Houdinipath = 'C:/Program Files/Side Effects Software/Houdini 19.5.569/bin/hindie.exe'
+
+    Houdinipath = 'C:/Program Files/Side Effects Software/Houdini 19.5.640/bin/houdinifx.exe'
     HoudiniScript = 'I:/GitHub/Tools/blender2houdini-exporter/houdini_import_fbx.py'
     export = None
-
+    
     def execute(self, context):
         # Verify the path and print it to the console
         fbxPath = 'I:/Temp_geo/model.fbx'
@@ -42,44 +43,50 @@ class SendToHoudini(bpy.types.Operator):
 
         # Check if Houdini is already running
         is_houdini_running = False
-        p = subprocess.Popen('tasklist', stdout=subprocess.PIPE, shell=True)
-        for line in p.stdout:
-            if b'houdini' in line.lower():
-                is_houdini_running = True
-                break
+        try:
+            output = subprocess.check_output('tasklist', shell=True)
+            output = output.decode('utf-8').lower()
+            is_houdini_running = re.search(r'houdinifx\.exe', output) is not None
+        except subprocess.CalledProcessError:
+            pass
         
         if not is_houdini_running:
             # launch Houdini
             cmd = [self.Houdinipath, '-c', 'python', self.HoudiniScript]
             subprocess.Popen(cmd)
         else:
-            # Check if Houdini's process is still alive
-            pid_file = os.path.join(os.environ['TEMP'], 'houdini.pid')
-            if os.path.exists(pid_file):
-                with open(pid_file) as f:
-                    pid = int(f.read().strip())
-                try:
-                    os.kill(pid, 0)
-                    print('Houdini is already running')
-                except OSError:
-                    # Process is not running anymore, launch Houdini
-                    os.remove(pid_file)
-                    cmd = [self.Houdinipath, '-c', 'python', self.HoudiniScript]
-                    subprocess.Popen(cmd)
-            else:
-                # Houdini is not running and PID file doesn't exist, launch Houdini
-                cmd = [self.Houdinipath, '-c', 'python', self.HoudiniScript]
-                subprocess.Popen(cmd)
+            print('Houdini is already running')
         
         return {'FINISHED'}
-        
+
+
+class HOUDINI_PT_Panel(bpy.types.Panel):
+    bl_idname = "HOUDINI_PT_Panel"
+    bl_label = "Houdini Menu"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Houdini'
+    
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("object.send_houdini", text="Send To Houdini")
+
+
+classes = (
+    SendToHoudini,
+    HOUDINI_PT_Panel,
+)
+
+
 def register():
-    bpy.utils.register_class(BlenderToHoudiniExporter)
-    bpy.utils.register_class(SendToHoudini)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
 
 def unregister():
-    bpy.utils.unregister_class(BlenderToHoudiniExporter)
-    bpy.utils.unregister_class(SendToHoudini)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     register()
